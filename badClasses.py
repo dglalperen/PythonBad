3aa8c930ac1ec25d11a39f4685ec3ff9f1099933
Alperen Dagli <alperen.dagli43@gmail.com>
2023-05-17 09:19:42 +0200

    initial

---
+++ badClasses.py
@@ -0,0 +1,14 @@
+# Badly named class
+class thing():
+    def __init__(self, a):
+        self.a = a
+
+# Badly named class
+class something():
+    def __init__(self, a):
+        self.a = a
+        
+# Badly named class
+class badclass():
+    def __init__(self, a):
+        self.a = a
---
+++ badFunctions.py
@@ -0,0 +1,11 @@
+# Badly named function
+def x(y, z):
+    return y + z
+
+# Badly named function
+def y(y, z):
+    return y + z
+
+# Badly named function
+def badFunctionForPython(y, z):
+    return y + z
